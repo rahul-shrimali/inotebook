0ccdc232985a272702d34e6e2116b2d9490d73d6
Rahul Shrimali <shrimalirahul00@gmail.com>
2021-12-20 11:34:59 +0530

    Adding and fetching notes endpoint

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 //Available routes
 app.use('/api/auth', require('./routes/auth'));
-app.use('/api/notes', require('./routes/auth'));
+app.use('/api/notes', require('./routes/notes'));
 
 
 app.listen(port, () => {
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,5 +1,47 @@
 const express = require('express')
 const router = express.Router();
+const fetchuser = require('../middleware/fetchuser');
+const Notes = require('../models/Notes');
+const { body, validationResult } = require('express-validator');
 
+//Route 1 : Get all the notes of user : GET "/api/notes/fetchallnotes". Login required
+router.get('/fetchallnotes', fetchuser, async (req, res)=>{
+    try {
+        const notes = await Notes.find({user : req.user.id});
+        res.json(notes);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal server occured");
+    }
+    
+})
+
+//Route 1 : Add a new note of user : POST "/api/notes/addnote". Login required
+router.post('/addnote', fetchuser, [
+    body('title', "Enter a valid title").isLength({min : 3}),
+    body('description', 'Description must be atleast 5 characters ').isLength({min : 5})
+],async (req, res)=>{
+        try {
+            const {title, description, tag} = req.body;
+
+
+            //If there are errors, return Bad request and errors 
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+
+            const note = new Notes({
+                title, description, tag, user : req.user.id
+            })
+
+            const savedNote = await note.save();
+
+            res.json(savedNote);
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send("Internal server occured");
+        }    
+})
 
 module.exports = router;
